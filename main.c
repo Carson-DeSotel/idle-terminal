76b2f68c876df630891c76bf0f39faf06ccb2bf6
Carson DeSotel <carson.desotel@gmail.com>
2021-01-14 21:09:51 -0600

    a program that utilizes the library to display a countdown in the terminal

--- main.c
+++ main.c
@@ -1,21 +1,83 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "lib/screen.h"
 #include "lib/color.h"
 #include "lib/cursor.h"
 
-int main(int argc, char* argv[]) {
-  char a[5] = {'H', 'E', 'L', 'L', 'O'};
+#define BORDER_HEIGHT 10
+#define BORDER_WIDTH  25
+#define BLOCK "\u2588"
+#define BG_COLOR BG_BLUE
 
-  clear(ENTIRE_SCREEN);
-
-  for(int i = 0; i < 5; i += 1) {
-    // cursor_down(i + 1);
-    // cursor_right(i + 1);
-    change_fg_bg(FG_BLACK, BG_GREEN);
-    change_mode(BOLD);
-    cursor_XY(i + 1, i + 1);
-    printf("%c", a[i]);
+void print_border() {
+  for(int i = 0; i < BORDER_HEIGHT; i += 1) {
+    change_bg(BG_COLOR);
+    for(int j = 0; j < BORDER_WIDTH; j += 1) {
+      if(i == 0 || i == BORDER_HEIGHT - 1) {
+        printf(BLOCK);
+      } else if((j == 0 || j == BORDER_WIDTH - 1)) {
+        printf(BLOCK);
+      } else {
+        printf(" ");
+      }
+    }
+    change_bg(RESET);
+    printf("\n");
   }
-  color_clear();
-  printf("\nDONE...\n");
+}
+
+void print_progress(int p) {
+  int bar_size = BORDER_WIDTH - 4;
+
+  change_bg(BG_COLOR);
+  cursor_save();
+  cursor_XY(2,2);
+  printf("[");
+  for(int j = 0; j < bar_size; j += 1) {
+    if(j <= p){ printf("#"); }
+    else      { printf("-"); }
+  }
+  printf("]");
+  cursor_load();
+  change_bg(RESET);
+}
+
+void print_loading(int p) {
+  p *= 5;
+  change_bg(BG_COLOR);
+  change_fg(FG_BLACK);
+  cursor_save();
+  cursor_XY(5,5);
+  printf("Now loading...%d%%", p);
+  cursor_load();
+  change_bg(RESET);
+}
+
+void print_finished() {
+  change_bg(BG_COLOR);
+  change_fg(FG_BLACK);
+  cursor_save();
+  cursor_XY(9,5);
+  printf("Finished!");
+  cursor_load();
+  change_bg(RESET);
+}
+
+int main(int argc, char* argv[]) {
+  for(int i = 1; i <= 21; i += 1) {
+    clear(ENTIRE_SCREEN);
+    print_border();
+    print_progress(i);
+
+    if(i != 21) {
+      print_loading(i);
+    } else {
+      print_finished();
+    }
+
+    fflush(stdout); // needed as no \n or other stream flushers are used.
+    sleep(1);
+  }
+
+  clear(SCREEN);
 }
