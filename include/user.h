a99f72a5ac86b4e24fb8c6a810f995f509694b98
Carson DeSotel <carson.desotel@gmail.com>
2021-01-19 10:09:41 -0600

    adds a user component to save gamestate

--- include/user.h
+++ include/user.h
@@ -1,1 +1,12 @@
-void configure_user();
+struct user {
+  int fg_color;
+  int max_level;
+  char filename[15];
+};
+
+#define FILENAME "user.dat"
+
+struct user user_init();
+void user_to_file(struct user u);
+struct user user_from_file();
+void user_print(struct user u);
--- src/user.c
+++ src/user.c
@@ -1,3 +1,35 @@
-void configure_user() {
-  
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/color.h"
+#include "../include/user.h"
+
+struct user user_init() {
+  struct user u = { FG_WHITE, 1, FILENAME };
+  return u;
+}
+
+void user_to_file(struct user u) {
+  FILE* fout;
+  fout = fopen(u.filename, "w");
+  fprintf(fout, "%d;%d;%s", u.fg_color, u.max_level, u.filename);
+  fclose(fout);
+}
+
+struct user user_from_file(char* filename) {
+  FILE* fin;
+  fin = fopen(filename, "r");
+  if(fin == NULL) {
+    printf("FILE NOT OPENED...\n");
+    exit(1);
+  }
+  struct user u = {0, 0};
+  fscanf(fin, "%d;%d;%s", &u.fg_color, &u.max_level, u.filename);
+
+  fclose(fin);
+  return u;
+}
+
+void user_print(struct user u) {
+  printf("{ \n\tfg_color: %d\n\tmax_level: %d\n\tfilename: %s\n}\n", u.fg_color, u.max_level, u.filename);
 }
