a2246d3d39936ca29acf02f2514c834727335f01
Carson DeSotel <carson.desotel@gmail.com>
2021-05-23 21:00:52 -0500

    adds string lib to headers

--- src/headers.h
+++ src/headers.h
@@ -1,9 +1,12 @@
 /* Standard C Headers: */
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 
 /* Defined Headers */
 #include "../include/bar.h"
 #include "../include/menu.h"
 #include "../include/idle-term.h"
 #include "../include/graphics.h"
+#include "../include/player.h"
